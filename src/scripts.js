6eadb97d18f76a5111187865eaa0b2a7e11026cf
leo_cesar <leoccet@gmail.com>
2021-08-19 14:27:35 -0300

    Finalizado ihuu

--- src/scripts.js
+++ src/scripts.js
@@ -1,51 +1,72 @@
 window.onload = () => {
-  let data, options, chart;
-  
+  let data, options, chart, table;
+
   google.charts.load( 'current', { 'packages': ['corechart'] } );
   google.charts.setOnLoadCallback( drawChart );
-  
-  function drawChart() {
-    data = google.visualization.arrayToDataTable([
-      ['Day', 'Backlog', "To Do", 'In Progress', 'Done'],
-      ['16/08/2021', 100, 50, 25, 25],
-      ['17/08/2021', 100, 60, 40, 35],
-      ['17/08/2021', 125, 60, 40, 10],
-    ]);
-  
-    options = {
-      title: 'CFD - Lab62',
 
-      hAxis: {
-        titleTextStyle: {
-          color: '#333'
-        }
+  setInterval(() => {
+    drawChart();
+   }, 60000);
+
+  options = {
+    title: 'CFD - Lab62',
+
+    hAxis: {
+      titleTextStyle: {
+        color: '#333'
+      },
+      slantedText: true,
+      slantedTextAngle: 45
+    },
+
+    vAxis: {
+      minValue: 0,
+    },
+
+    series: {
+      0: {
+        color: '#304ffe',
       },
 
-      vAxis: {
-        minValue: 0,
+      1: {
+        color: '#ffd600',
       },
 
-      series: {
-        0: {
-          color: '#304ffe',
-        },
-
-        1: {
-          color: '#ffd600',
-        },
-
-        2: {
-          color: '#00c853',
-        }
+      2: {
+        color: '#00c853',
       }
-    };
-  
-    chart = new google.visualization.AreaChart( document.getElementById('chart') );
-  
-    chart.draw( data, options );
-  }
-  
-  window.onresize = () => {
-    chart.draw( data, options );
+    }
   };
+
+  function drawChart() {
+    data = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1j5Yl5-lk8oJcucet0jnVO7lm-86KRgxe29ds2-hJnHc/gviz/tq?range=A1:D');
+
+    data.send( processaDados );
+
+    function processaDados( resultado ) {
+      table = resultado.getDataTable();
+
+      table = google.visualization.arrayToDataTable( processaCsv(google.visualization.dataTableToCsv( table )) );
+
+      chart = new google.visualization.AreaChart( document.getElementById('chart') );
+      chart.draw( table, options );
+    };
+
+    
+  };
+
+    function processaCsv( csv ) {
+      csv = csv.split('\n');
+      csv = csv.map(item => {
+        return item.split(',');
+      });
+
+      csv = csv.filter(item => item[1] !== '' );
+      csv = csv.filter(item => item[0] !== '' );
+      csv = csv.map(item => [ item[0], parseInt(item[1]), parseInt(item[2]), parseInt(item[3])])
+      csv = [['Dia', 'A fazer', 'Fazendo', 'Feito'], ...csv]
+
+      return csv;
+    }
+
 };
